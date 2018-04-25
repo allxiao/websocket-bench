aca6a249a581e1542e28d15df2208d09ea673a0c
Menghua Xiao <menxiao@microsoft.com>
2018-04-25 14:08:42 +0800

    Protocol update for the latest SignalR service

--- benchmark/signalr_common.go
+++ benchmark/signalr_common.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"regexp"
+	"net/url"
 	"strconv"
 	"strings"
 	"time"
@@ -19,6 +19,22 @@ import (
 type SignalrServiceHandshake struct {
 	ServiceUrl string `json:"serviceUrl"`
 	JwtBearer  string `json:"accessToken"`
+}
+
+type SignalRTransport struct {
+	Transport       string   `json:"transport"`
+	TransferFormats []string `json:"transferFormats"`
+}
+
+type SignalrServerNegotiation struct {
+	URL                 string             `json:"url"`
+	AccessToken         string             `json:"accessToken"`
+	AvailableTransports []SignalRTransport `json:"availableTransports"`
+}
+
+type SignalrServiceNegotiation struct {
+	ConnectionID        string             `json:"connectionId"`
+	AvailableTransports []SignalRTransport `json:"availableTransports"`
 }
 
 type SignalrCoreCommon struct {
@@ -86,7 +102,7 @@ func (s *SignalrCoreCommon) SignalrServiceBaseConnect(protocol string) (session 
 		return
 	}
 
-	negotiateResponse, err := http.Get("http://" + s.host + "/signalr/chat")
+	negotiateResponse, err := http.Post("http://"+s.host+"/chat/negotiate", "text/plain;charset=UTF-8", strings.NewReader(""))
 	if err != nil {
 		s.LogError("connection:error", id, "Failed to negotiate with the server", err)
 		return
@@ -94,16 +110,51 @@ func (s *SignalrCoreCommon) SignalrServiceBaseConnect(protocol string) (session 
 	defer negotiateResponse.Body.Close()
 
 	decoder := json.NewDecoder(negotiateResponse.Body)
-	var handshake SignalrServiceHandshake
-	err = decoder.Decode(&handshake)
+	var negotiation SignalrServerNegotiation
+	err = decoder.Decode(&negotiation)
 	if err != nil {
-		s.LogError("connection:error", id, "Failed to decode service URL and jwtBearer", err)
+		s.LogError("connection:error", id, "Failed to decode server negotiation response", err)
 		return
 	}
 
-	var httpPrefix = regexp.MustCompile("^https?://")
-	baseURL := httpPrefix.ReplaceAllString(handshake.ServiceUrl, "wss://")
-	wsURL := baseURL + "&access_token=" + handshake.JwtBearer
+	u, err := url.Parse(negotiation.URL)
+	if err != nil {
+		s.LogError("connection:error", id, "Failed to decode service URL", err)
+		return
+	}
+
+	u.Path = u.Path + "/negotiate"
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(""))
+	if err != nil {
+		s.LogError("connection:error", id, "Failed to create POST request to SignalR service", err)
+		return
+	}
+	req.Header.Set("Content-Type", "text/plain;charset=UTF-8")
+	req.Header.Set("Authorization", "Bearer "+negotiation.AccessToken)
+
+	serviceNegotiationResponse, err := http.DefaultClient.Do(req)
+	if err != nil {
+		s.LogError("connection:error", id, "Failed to post negotiate request to SignalR service", err)
+		return
+	}
+	defer serviceNegotiationResponse.Body.Close()
+
+	decoder = json.NewDecoder(serviceNegotiationResponse.Body)
+	var serviceNegotiation SignalrServiceNegotiation
+	err = decoder.Decode(&serviceNegotiation)
+	if err != nil {
+		s.LogError("connection:error", id, "Failed to decode service negotiation response", err)
+		return
+	}
+
+	u, _ = url.Parse(negotiation.URL)
+	query := u.Query()
+	query.Set("access_token", negotiation.AccessToken)
+	query.Set("id", serviceNegotiation.ConnectionID)
+	u.RawQuery = query.Encode()
+	u.Scheme = "wss"
+
+	wsURL := u.String()
 
 	c, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
 	if err != nil {
